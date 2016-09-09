acb432dee83456f254459999a352f644c8981223
Alexander Tkachev <alex.leksat@gmail.com>
2016-09-09 16:02:58 +0300

    Ignore "page" and all the empty query parameters. HEKS-136

--- src/Plugin/Block/CurrentSearchResetLink.php
+++ src/Plugin/Block/CurrentSearchResetLink.php
@@ -68,6 +68,22 @@ class CurrentSearchResetLink extends CurrentSearchBase {
     $request = \Drupal::request();
     $current_url = $request->getSchemeAndHttpHost() . $request->getRequestUri();
 
+    // Ignore "page" and all the empty query parameters.
+    $parsed = parse_url($current_url);
+    parse_str(isset($parsed['query']) ? $parsed['query'] : '', $query);
+    foreach ($query as $key => $value) {
+      if ($key == 'page' || $value === '') {
+        unset($query[$key]);
+      }
+    }
+    if (count($query)) {
+      $parsed['query'] = http_build_query($query);
+    }
+    else {
+      unset($parsed['query']);
+    }
+    $current_url = $this->buildUrl($parsed);
+
     if ($current_url != $reset_url->setAbsolute()->toString()) {
       $build = [
         '#type' => 'html_tag',
@@ -82,6 +98,29 @@ class CurrentSearchResetLink extends CurrentSearchBase {
     }
 
     return $build;
+  }
+
+  /**
+   * Builds a URL from the parse_url() result.
+   *
+   * Taken from http://php.net/manual/en/function.parse-url.php#106731
+   *
+   * @param array $parsed_url
+   *   The result of parse_url().
+   *
+   * @return string
+   */
+  public function buildUrl($parsed_url) {
+    $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+    $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+    $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+    $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+    $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
+    $pass     = ($user || $pass) ? "$pass@" : '';
+    $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+    $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+    $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+    return "$scheme$user$pass$host$port$path$query$fragment";
   }
 
 }
